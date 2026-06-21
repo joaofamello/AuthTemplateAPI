3c2873cce1ddb0d615baf46799644f00ca696efe
João Francisco <joaofrancisco.amello@gmail.com>
2026-06-21 12:23:18 -0300

    feat: criando classe de configuração jwt e configurando o program.cs

--- AuthTemplateAPI/Program.cs
+++ AuthTemplateAPI/Program.cs
@@ -1,8 +1,60 @@
+using System.Text;
+using AuthTemplateAPI.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// ----------------------------- Configuração do JWT -----------------------------
+
+// 1. Mapeia a seção do appsettings.json para a classe JwtSettings
+var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+builder.Services.Configure<JwtSettings>(jwtSettingsSection);
+
+// Converte a seção para usar as variáveis locais na configuração do JwtBearer abaixo
+var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+var key = Encoding.ASCII.GetBytes(jwtSettings!.SecretKey);
+
+// 2. Configura o serviço de Autenticação do .NET
+builder.Services.AddAuthentication(options =>
+{
+    // Define que o padrão de autenticação e desafio da API será via Token (Bearer)
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false; // Em produção, mudar para true
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        ClockSkew = TimeSpan.Zero // remove o tempo de tolerância padrão de 5 minutos do token expirado
+    };
+});
+
+// ---------------------------------------------------------------------------
+
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
