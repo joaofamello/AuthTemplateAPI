56d9ce812c9d0d7d0d8108c9fd407f299052ecc3
João Francisco <joaofrancisco.amello@gmail.com>
2026-06-21 21:30:44 -0300

    test(TokenService): adição de teste unitário

---
+++ AuthTemplateAPI.Tests/TokenServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using AuthTemplateAPI.Models;
+using AuthTemplateAPI.Services;
+using AuthTemplateAPI.Settings;
+using Microsoft.Extensions.Options;
+
+namespace AuthTemplateAPI.Tests;
+
+public class TokenServiceTests
+{
+    private TokenService CriarServico()
+    {
+        var settings = new JwtSettings
+        {
+            SecretKey = "ChaveDeTesteComPeloMenos32Caracteres!",
+            Issuer = "AuthTemplateAPI.Tests",
+            Audience = "ClienteDeTeste",
+            ExpirationMinutes = 60
+        };
+
+        return new TokenService(Options.Create(settings));
+    }
+
+    [Fact]
+    public void GenerateToken_DeveIncluirClaimsDeUsername_EId()
+    {
+        var servico = CriarServico();
+        var usuario = new Usuario { Id = "abc-123", UserName = "teste" };
+        
+        var token  = servico.GenerateToken(usuario);
+
+        var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+        
+        Assert.Equal("teste", jwt.Claims.First(c => c.Type == "username").Value);
+        Assert.Equal("abc-123", jwt.Claims.First(c => c.Type == "id").Value);
+        Assert.Equal("AuthTemplateAPI.Tests", jwt.Issuer);
+    }
+}
